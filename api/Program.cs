5d9149f6227acfedcdb810da95b32cf6f11c2b44
Headphones <mlemos@apoioprodesp.sp.gov.br>
2023-01-12 13:54:48 -0300

    inclusão do prometheus e elasticsearch

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,6 @@
+using Prometheus;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// elasticsearch
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200/"))
+    {
+        AutoRegisterTemplate = true,
+    })
+    .CreateLogger();
+
+builder.Services.AddLogging(b => b.AddSerilog());
+
 var app = builder.Build();
+
+/*INICIO DA CONFIGURAÇÃO - PROMETHEUS*/
+// Custom Metrics to count requests for each endpoint and the method
+var counter = Metrics.CreateCounter("webapimetric", "Counts requests to the WebApiMetrics API endpoints",
+    new CounterConfiguration
+    {
+        LabelNames = new[] { "method", "endpoint" }
+    });
+
+app.Use((context, next) =>
+{
+    counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+    return next();
+});
+
+// Use the prometheus middleware
+app.UseMetricServer();
+app.UseHttpMetrics();
+
+/*FIM DA CONFIGURAÇÃO - PROMETHEUS*/
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
